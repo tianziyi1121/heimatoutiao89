e42fc8aad3d030e76fbc9de7c40cc3243d2bf81a
tianziyi <tianziyi@qq.com>
2019-12-19 00:21:35 +0800

    前端校验成功

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,7 +6,7 @@
         <img src="../../assets/img/logo_index.png" alt />
       </div>
       <!-- 表单容器  登录表单 -->
-      <el-form :model="loginForm" :rules="loginRules" style="margin-top:30px">
+      <el-form ref="myForm" :model="loginForm" :rules="loginRules" style="margin-top:30px">
         <!-- 表单域 必须写在el-form里面 -->
         <el-form-item prop="mobile">
           <!-- 里面放具体的组件 里面是放element ui的标签 -->
@@ -20,7 +20,7 @@
           <el-checkbox v-model="loginForm.check">我已经阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button @click="submitLogin" type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -37,8 +37,34 @@ export default {
         check: false // 是否勾选，false不勾选
       },
       loginRules: {
-        // 验证规则 验证登录表单的
+        // 验证规则 验证登录表单的 key(字段名):value(数组)数组里面放对象
+        mobile: [{
+          required: true, message: '请输入你的手机号' }, // required要求必填,不能为空
+        { pettern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }], // 用正则表达式来要求输入手机号的规则 若不正确,给出提示信息
+        code: [{ required: true, message: '请输入验证码' }, {
+          pettern: /^\d{6}$/, message: '验证格式不正确'
+        }],
+        check: [{ validator: function (rule, value, callback) {
+          // rule指当前的规则,没什么用
+          // value 指要校验的字段的值,
+          // callback 指校验成功或者失败后要执行的回调函数
+          if (value) {
+            // 校验通过  直接执行callback回调函数
+            callback()
+          } else {
+            // 校验不通过 要提示信息
+            callback(new Error('出错了,你必须同意'))
+          }
+        } }]
+
       }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(function (isOK) {
+        // 前端校验成功
+      })
     }
   }
 }
