d99e88c65ad16027f4ec3e7b948a75b50a790a8b
tianziyi <tianziyi@qq.com>
2019-12-18 18:13:55 +0800

    lll

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from '/element-ui' // 引入包
+import './styles/index.less' // 引入初始化样式
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 
 Vue.config.productionTip = false
+Vue.use(ElementUI) // 全局注册
 
 new Vue({
   router,
