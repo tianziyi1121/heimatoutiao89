31c299a06171d8798667ad7ade4b34e9b9d37990
tianziyi <tianziyi@qq.com>
2019-12-31 01:36:26 +0800

    账户信息 页面结构完成

--- src/router/index.js
+++ src/router/index.js
@@ -16,40 +16,48 @@ const routes = [
     path: '/home',
     name: 'hoem',
     component: Home,
-    children: [{
-      path: '', // 二级路由，什么都不写，作为显示组件
-      component: Home2
-    }, {
-      // 二级路由表
-      // path: '../views/comment', // 完整地址
-      path: 'comment', // 相对地址
-      component: () => import('../views/comment/index.vue')// 按需加载
-      // component: Commont
-    }, {
-      path: 'material', // 素材列表管理
-      component: () => import('../views/material/index.vue')// 按需加载
-    },
-    {
-      path: 'articles', // 相对地址
-      component: () => import('../views/articles/index.vue')// 按需加载
-      // component: Commont
-    },
-    {
-      path: 'publish/:articleId', // 相对地址   //此规则只匹配修改文章
-      component: () => import('../views/publish/index.vue')// 按需加载
-      // component: Commont
-    },
-    {
-      path: 'publish', // 相对地址   //此规则只匹配新增发布文章
-      component: () => import('../views/publish/index.vue')// 按需加载
-      // component: Commont
-    }
-      //   {
-      //     path: 'account', // 账户信息
-      //     component: () => import('../views/account') // 按需加载
-      //   }]
-      // }+++++++
+    children: [
+      {
+        path: '', // 二级路由，什么都不写，作为显示组件
+        component: Home2
+      },
+      {
+        // 二级路由表
+        // path: '../views/comment', // 完整地址
+        path: 'comment', // 相对地址
+        component: () => import('../views/comment/index.vue')// 按需加载
+        // component: Commont
+      },
+      {
+        path: 'material', // 素材列表管理
+        component: () => import('../views/material/index.vue')// 按需加载
+      },
+      {
+        path: 'articles', // 相对地址
+        component: () => import('../views/articles/index.vue')// 按需加载
+        // component: Commont
+      },
+      {
+        path: 'publish/:articleId', // 相对地址   //此规则只匹配修改文章
+        component: () => import('../views/publish/index.vue')// 按需加载
+        // component: Commont
+      },
+      {
+        path: 'publish', // 相对地址   //此规则只匹配新增发布文章
+        component: () => import('../views/publish/index.vue')// 按需加载
+        // component: Commont
+      },
+      {
+        path: 'account', // 相对地址   //此规则只匹配新增发布文章
+        component: () => import('../views/account/index.vue')// 按需加载
+      }
     ]
+
+  },
+
+  {
+    path: 'account', // 账户信息
+    component: () => import('../views/account') // 按需加载
 
   },
   {
@@ -66,9 +74,7 @@ const routes = [
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   // }
 ]
-
 const router = new VueRouter({
   routes
 })
-
 export default router
